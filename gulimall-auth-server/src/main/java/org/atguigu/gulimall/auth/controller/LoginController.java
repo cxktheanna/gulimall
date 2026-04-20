66b3dd221ce5f61475e1e4937bf935d60a33b798
dcq01 <daichaoqun01@gmail.com>
2026-04-20 20:32:50 +0800

    引入分布式Session 不同域名下均可获取登录信息

--- gulimall-auth-server/src/main/java/org/atguigu/gulimall/auth/controller/LoginController.java
+++ gulimall-auth-server/src/main/java/org/atguigu/gulimall/auth/controller/LoginController.java
@@ -43,22 +43,22 @@ public class LoginController {
     @Autowired
     StringRedisTemplate redisTemplate;
 
-//    /**
-//     * 访问登录页面
-//     * 登录状态自动跳转首页
-//     */
-//    @GetMapping(value = "/login.html")
-//    public String loginPage(HttpSession session) {
-//        // 判断是否登录状态
-//        Object attribute = session.getAttribute(AuthConstant.LOGIN_USER);
-//        if (attribute == null) {
-//            // 未登录，返回登录页资源
-//            return "login";
-//        } else {
-//            // 已登录
-//            return "redirect:http://gulimall.com";
-//        }
-//    }
+    /**
+     * 访问登录页面
+     * 登录状态自动跳转首页
+     */
+    @GetMapping(value = "/login.html")
+    public String loginPage(HttpSession session) {
+        // 判断是否登录状态
+        Object attribute = session.getAttribute(AuthConstant.LOGIN_USER);
+        if (attribute == null) {
+            // 未登录，返回登录页资源
+            return "login";
+        } else {
+            // 已登录
+            return "redirect:http://gulimall.com";
+        }
+    }
 
     /**
      * 发送短信验证码
